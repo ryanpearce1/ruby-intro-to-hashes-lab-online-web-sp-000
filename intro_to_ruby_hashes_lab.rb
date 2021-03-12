76b801f24b0c0378af605621ef3c9f25e9ec4f71
Ryan Pearce <ryan.pearce@key.me>
2021-03-12 21:49:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,8 +36,8 @@ def monopoly_with_third_tier
 	  :railroads => {
 	     :pieces => 4,
 	     :names => {
-	       :reading_railroade => {},
-	       :pennsyulvania_railroad => {},
+	       :reading_railroad => {},
+	       :pennsylvania_railroad => {},
 	       :b_and_o_railroad => {},
 	       :shortline => {},
 	     },
